b75118c57af198ab3dd6ea0a1be99d1b449bd1cc
kr37577 <jiatengl127@gmail.com>
2025-05-24 01:46:38 +0900

    add：タスクのためのSQLAlchemyモデルを定義

---
+++ back/app/models/database_models.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String, Boolean, DateTime
+from sqlalchemy.sql import func
+from ..database.database import Base
+
+
+class Task(Base):
+    __tablename__ = "tasks"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    title = Column(String, index=True, nullable=False)
+    description = Column(String, index=True, nullable=True)
+    is_completed = Column(Boolean, default=False, nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+    def __repr__(self):
+        return f"<Task(id={self.id}, title='{self.title}')>"
